9d150c50a608532c05cdc5fe66c60c2e934e53d9
abdollah raafat <abdollah.raafat.daif@gmail.com>
2022-04-28 09:57:33 +0200

    Add home tests file

---
+++ src/__tests__/__snapshots__/Home.test.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Home from '../../components/Home';
+
+it('render correctly', () => {
+  const elements = renderer.create(<Home />).toJSON();
+
+  expect(elements).toMatchSnapshot();
+});
